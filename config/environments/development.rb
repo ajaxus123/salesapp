898e6d300be88868fba83e5b5a8a8db45d7fff0b
Michael Bekker <bekker.michael2@gmail.com>
2016-12-23 10:33:45 +0000

    added admin, order_clerk, return_clerk and manager roles to users trhough a migration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Disable Error Logger
+  config.logger = ActiveSupport::Logger.new(nil)
+
   # Needed for Devise in Development
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
@@ -43,7 +46,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
